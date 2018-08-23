df2cec1e4b4018ae5a398909b846687e5712eadb
viva-jasper <36062327+viva-jasper@users.noreply.github.com>
2018-08-23 16:20:36 +0100

    Update app.cpp

--- source/app.cpp
+++ source/app.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[]) {
   v1::RTSP rtsp("169.254.124.38","554");
   rtsp.setStreamPath("/Streaming/Channels/");
   rtsp.setStreamChannel("101");
-  rtsp.setStreamAuthentication("admin","000boondocks");
+  rtsp.setStreamAuthentication("admin","admin123");
   rtsp.run();
 
   return 0;
